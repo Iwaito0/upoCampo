6a6def96d37b8e5557f5578e0ef34166040d5d3e
Iwaito0 <57145421+Iwaito0@users.noreply.github.com>
2020-01-11 19:53:37 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -3,6 +3,12 @@
 var oUPOCampo = new UpoCampo();
 var divListado = document.getElementById("listado");
 
+var boton=document.getElementById("btnAceptar");
+boton.addEventListener("click",prueba);
+
+function prueba(){
+    alert("asdasd");
+}
 //Mostrar Formularios
 
 //Mostrar formulario cliente(anadir los otros formularios mientras los vais creando)
