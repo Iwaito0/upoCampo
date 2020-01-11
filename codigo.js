d4328f6536922ac01f019ed23b15c31b46ce76eb
Salvador-B <59707016+Salvador-B@users.noreply.github.com>
2020-01-11 20:56:19 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -3,15 +3,39 @@
 var oUPOCampo = new UpoCampo();
 var divListado = document.getElementById("listado");
 
-var boton=document.getElementById("btnAceptar");
-boton.addEventListener("click",prueba);
+/*--------------EVENTOS DE MENU----------------*/
 
-function prueba(){
+var menuAltaCliente = document.getElementById("altaCliente");
+menuAltaCliente.addEventListener("click", altaCliente, false);
+
+var menuAltaReserva = document.getElementById("altaReserva");
+menuAltaReserva.addEventListener("click", altaReserva, false);
+
+
+
+var botonAltaCliente = document.getElementById("btnAceptarAltaCliente");
+botonAltaCliente.addEventListener("click", aceptarAltaCliente, false);
+
+var botonAltaReserva = document.getElementById("btnAceptarAltaReserva");
+botonAltaReserva.addEventListener("click", aceptarAltaReserva, false);
+
+function aceptarAltaCliente(){
     alert("asdasd");
 }
+
+function aceptarAltaReserva(){
+    alert("abcd");
+}
+
 //Mostrar Formularios
 
 //Mostrar formulario cliente(anadir los otros formularios mientras los vais creando)
 function altaCliente() {
     frmAltaCliente.style.display = "block";
+    frmAltaReserva.style.display = "none";
+}
+
+function altaReserva() {
+	frmAltaReserva.style.display = "block";
+	frmAltaCliente.style.display = "none";
 }
