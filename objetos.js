532991bda8ab5eabb03e7a5bca65132437918cd9
Iwaito0 <57145421+Iwaito0@users.noreply.github.com>
2020-01-13 18:25:57 +0100

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -10,6 +10,17 @@ Cliente.prototype.toString = function() {
     let sMensaje = "El cliente " +this.nombre+ " " +this.apellido+" con DNI " +this.nif+ " y telefono " +this.telefono;
     return sMensaje;
 }
+//Clase Habitacion
+function Habitacion(iID, sTipo, fPrecio, iOcupacionMax) {
+    this.id = iID;
+    this.tipo= sTipo;
+    this.precio = fPrecio;
+    this.ocupacionMaxima = iOcupacionMax;
+}
+Habitacion.prototype.toString = function() {
+    let sMensaje = "La habitacion " +this.id+ " del tipo " +this.tipo+" con un precio de " +this.precio+ " y una ocupacion maxima de " +this.ocupacionMaxima;
+    return sMensaje;
+}
 
 //Clase principal
 class UpoCampo{
