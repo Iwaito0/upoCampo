ea89f6260926a44d57bda765687f79fd67344999
Iwaito0 <57145421+Iwaito0@users.noreply.github.com>
2020-01-13 18:31:04 +0100

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -21,7 +21,22 @@ Habitacion.prototype.toString = function() {
     let sMensaje = "La habitacion " +this.id+ " del tipo " +this.tipo+" con un precio de " +this.precio+ " y una ocupacion maxima de " +this.ocupacionMaxima;
     return sMensaje;
 }
-
+//Clase Parking
+function Parking(iID, fPrecio, bAnchoEspecial) {
+    this.id = iID;
+    this.precio = fPrecio;
+    this.anchoespecial = bAnchoEspecial;
+}
+Parking.prototype.toString = function() {
+    if(this.anchoespecial==true){
+    let sMensaje = "El parking " +this.id+ " con un precio de " +this.precio+ " y con ancho especial";
+    return sMensaje;
+    }
+    else{
+    let sMensaje = "El parking " +this.id+ " con un precio de " +this.precio+ " y sin ancho especial";
+    return sMensaje;
+    }
+}
 //Clase principal
 class UpoCampo{
 
