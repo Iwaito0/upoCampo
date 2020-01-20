bdc2b9ae6eb4e5fbbb17b3d04f7983fd8d383ab6
Iwaito0 <57145421+Iwaito0@users.noreply.github.com>
2020-01-20 20:01:51 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -110,6 +110,7 @@ botonModificarReserva.addEventListener("click", aceptarModificarReserva, false);
 /*-------------FUNCIONES-----------------*/
 
 function aceptarAltaCliente(){
+    let sMensaje="";
     // Recoger valores del formulario
     let sDni = frmAltaCliente.txtNifAlta.value.trim();
     let sNombre = frmAltaCliente.txtNombreClienteAlta.value.trim();
@@ -118,14 +119,76 @@ function aceptarAltaCliente(){
     let sEmail = frmAltaCliente.txtEmailAlta.value.trim();
     let iNumTarjeta = parseInt(frmAltaCliente.txtNTarjetaAlta.value.trim());
 
+    if(!/^\d{8}[a-zA-Z]$/.test(sDni)){
+        sMensaje+="El campo DNI esta mal\n";
+        frmAltaCliente.txtNifAlta.classList.add("error");
+    }
+    else{
+        frmAltaCliente.txtNifAlta.classList.remove("error");  
+    }
+
+    if(!/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(sNombre)){
+        sMensaje+="El campo nombre esta mal\n";
+        frmAltaCliente.txtNombreClienteAlta.classList.add("error");
+    }
+    else{
+        frmAltaCliente.txtNombreClienteAlta.classList.remove("error");  
+    }
+
+
+    if(!/^(\+34|0034|34)?[6|7|9][0-9]{8}$/g.test(iTelefono)){
+        sMensaje+="El campo telefono esta mal (Recuerde poner el prefijo )\n";
+        frmAltaCliente.txtTelefonoClienteAlta.classList.add("error");
+    }
+    else{
+        frmAltaCliente.txtTelefonoClienteAlta.classList.remove("error");  
+    }
+    
+    if(!/[a-zA-Z1-9À-ÖØ-öø-ÿ]+\.?(( |\-)[a-zA-Z1-9À-ÖØ-öø-ÿ]+\.?)*/.test(sDireccion)){
+        sMensaje+="El campo direccion esta mal\n";
+        frmAltaCliente.txtDireccionAlta.classList.add("error");
+    }
+    else{
+        frmAltaCliente.txtDireccionAlta.classList.remove("error");  
+    }
+
+    if(!/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g.test(sEmail)){
+        sMensaje+="El campo email esta mal\n";
+        frmAltaCliente.txtEmailAlta.classList.add("error");
+    }
+    else{
+        frmAltaCliente.txtEmailAlta.classList.remove("error");  
+    }
+    if(!/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35d{3})d{11})$/.test(iNumTarjeta)){
+        sMensaje+="El campo del numero de la tarjeta esta mal\n";
+        frmAltaCliente.txtNTarjetaAlta.classList.add("error");
+    }
+    else{
+        frmAltaCliente.txtNTarjetaAlta.classList.remove("error");  
+    }
+
+    if(sMensaje==""){
+    // Creamos el objeto cliente
+    let oCliente = new Cliente(sDni, sNombre, iTelefono, sDireccion, sEmail, iNumTarjeta);
+
+    // Alta de cliente en UPOCAMPO
+    sMensaje = oUPOCampo.altaCliente(oCliente);
+    alert(sMensaje);    
+    frmAltaCliente.reset();    
+    }
+    else{
+        alert(sMensaje);
+    }
+
+
+    /*
     // Creamos el objeto cliente
     let oCliente = new Cliente(sDni, sNombre, iTelefono, sDireccion, sEmail, iNumTarjeta);
 
     // Alta de cliente en UPOCAMPO
     let sMensaje = oUPOCampo.altaCliente(oCliente);
-
-    alert(sMensaje);
-    frmAltaCliente.reset();
+*/
+    //frmAltaCliente.reset();
 }
 
 function aceptarAltaReserva(){
