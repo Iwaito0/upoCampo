ad9d822f2cfac4b227e49aba50e9fd1b1e24e4f5
Salvador-B <59707016+Salvador-B@users.noreply.github.com>
2020-01-22 21:47:46 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -3,6 +3,8 @@
 var oUPOCampo = new UpoCampo();
 var divListado = document.getElementById("listado");
 
+datosHabitaciones();
+
 /*--------------EVENTOS DE MENU----------------*/
 
 /*---------------MENU ALTA-------------*/
@@ -186,6 +188,30 @@ function aceptarAltaCliente(){
     }
 }
 
+function mostrarHabitaciones()  {
+    document.getElementById("selectListaHab").length = 0;
+    let aReserva = oUPOCampo.getArrayReservas();
+    let aHabitaciones = oUPOCampo.getArrayHabitaciones();
+
+    for (let i = 0; i < aHabitaciones.length; i++) {
+        for (let j = 0; j < aReserva.length; j++) {
+            if (aHabitaciones[i].id == aReserva[j].numHabitaciones) {
+                aHabitaciones.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    for (let i = 0; i < aHabitaciones.length; i++) {
+        let opc = document.createElement("option");
+        opc.setAttribute("value", aHabitaciones[i].id);
+        let texto = document.createTextNode(aHabitaciones[i].id);
+        opc.appendChild(texto);
+        document.getElementById("selectListaHab").appendChild(opc);
+    }
+    
+}
+
 function aceptarAltaReserva(){
     let sMensaje="";
 
@@ -195,7 +221,7 @@ function aceptarAltaReserva(){
     let dCheckin = frmAltaReserva.txtEntradaAlta.value.trim();
     let dCheckout = frmAltaReserva.txtSalidaAlta.value.trim();
     let fPrecio = parseFloat(frmAltaReserva.txtPrecioAlta.value.trim());
-
+    let iNumHabitacion = parseInt(frmAltaReserva.selectListaHab.value.trim());
 
     if(!/^\d+$/.test(iID)){
         sMensaje+="El campo ID esta mal. El campo ID debe ser un numero\n";
@@ -219,24 +245,18 @@ function aceptarAltaReserva(){
         frmAltaReserva.txtPrecioAlta.classList.remove("error");  
     }
 
-
-
     if(sMensaje==""){
     // Creamos el objeto reserva
-    let oReserva = new Reservas(iID, iNumPersonas, dCheckin, dCheckout, fPrecio);
+    let oReserva = new Reservas(iID, iNumPersonas, dCheckin, dCheckout, fPrecio, iNumHabitacion);
     // Alta de reserva en UPOCAMPO
     let sMensaje = oUPOCampo.altaReserva(oReserva);
-    alert(sMensaje);    
+    alert(sMensaje);
+    mostrarHabitaciones();    
     frmAltaReserva.reset();    
     }
     else{
         alert(sMensaje);
     }
-    
-
-
-    
-    
 }
 
 function aceptarAltaProveedor(){
@@ -775,6 +795,9 @@ function listadosReservas(){
     
     oCelda=oFila.insertCell(-1);
     oCelda.textContent="Precio";
+
+    oCelda=oFila.insertCell(-1);
+    oCelda.textContent="Numero de habitacion";
     
     //El cuerpo de la tabla
     let oTBody = document.createElement("TBODY");
@@ -798,11 +821,14 @@ function listadosReservas(){
         oCelda = oFila.insertCell(-1);
         oCelda.textContent = arrayReservas[i].precio;
         
-        
+        oCelda = oFila.insertCell(-1);
+        oCelda.textContent = arrayReservas[i].numHabitaciones;
     }
     
     pestana.document.body.append(oTabla);
 }
+
+
 
 //Mostrar Formularios
 
@@ -827,6 +853,7 @@ function mostrarAltaReserva() {
 	frmModificarCliente.style.display = "none";
     frmModificarReserva.style.display = "none";
     frmAltaActividades.style.display = "none";
+    mostrarHabitaciones();
 
 }
 
@@ -901,85 +928,96 @@ function mostrarAltaActividad(){
 }
 //Datos prueba de habitaciones
 
-oUPOCampo.altaHabitacion(new Habitacion(0, "simple", 28.90, 2));
-oUPOCampo.altaHabitacion(new Habitacion(1, "doble", 40.50, 4));
-oUPOCampo.altaHabitacion(new Habitacion(2, "simple", 28.90, 2));
-oUPOCampo.altaHabitacion(new Habitacion(3, "doble", 40.50, 4));
-oUPOCampo.altaHabitacion(new Habitacion(4, "simple", 28.90, 2));
-oUPOCampo.altaHabitacion(new Habitacion(5, "doble", 40.50, 4));
-oUPOCampo.altaHabitacion(new Habitacion(6, "doble", 40.50, 4));
-oUPOCampo.altaHabitacion(new Habitacion(7, "doble", 40.50, 4));
-oUPOCampo.altaHabitacion(new Habitacion(8, "simple", 28.90, 2));
-oUPOCampo.altaHabitacion(new Habitacion(9, "simple", 28.90, 2));
-oUPOCampo.altaHabitacion(new Habitacion(10, "simple", 28.90, 2));
-oUPOCampo.altaHabitacion(new Habitacion(11, "simple", 28.90, 2));
-oUPOCampo.altaHabitacion(new Habitacion(12, "doble", 40.50, 4));
-oUPOCampo.altaHabitacion(new Habitacion(13, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(14, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(15, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(16, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(17, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(18, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(19, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(20, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(21, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(22, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(23, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(24, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(25, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(26, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(27, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(28, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(29, "triple", 75.95, 6));
-oUPOCampo.altaHabitacion(new Habitacion(30, "premium", 149.99, 2));
-oUPOCampo.altaHabitacion(new Habitacion(31, "premium", 149.99, 2));
-oUPOCampo.altaHabitacion(new Habitacion(32, "premium", 149.99, 2));
-oUPOCampo.altaHabitacion(new Habitacion(33, "premium", 149.99, 2));
-oUPOCampo.altaHabitacion(new Habitacion(34, "premium", 149.99, 2));
-oUPOCampo.altaHabitacion(new Habitacion(35, "premium", 149.99, 2));
+function datosHabitaciones() {
+    oUPOCampo.altaHabitacion(new Habitacion(0, "simple", 28.90, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(1, "doble", 40.50, 4));
+    oUPOCampo.altaHabitacion(new Habitacion(2, "simple", 28.90, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(3, "doble", 40.50, 4));
+    oUPOCampo.altaHabitacion(new Habitacion(4, "simple", 28.90, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(5, "doble", 40.50, 4));
+    oUPOCampo.altaHabitacion(new Habitacion(6, "doble", 40.50, 4));
+    oUPOCampo.altaHabitacion(new Habitacion(7, "doble", 40.50, 4));
+    oUPOCampo.altaHabitacion(new Habitacion(8, "simple", 28.90, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(9, "simple", 28.90, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(10, "simple", 28.90, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(11, "simple", 28.90, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(12, "doble", 40.50, 4));
+    oUPOCampo.altaHabitacion(new Habitacion(13, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(14, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(15, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(16, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(17, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(18, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(19, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(20, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(21, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(22, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(23, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(24, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(25, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(26, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(27, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(28, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(29, "triple", 75.95, 6));
+    oUPOCampo.altaHabitacion(new Habitacion(30, "premium", 149.99, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(31, "premium", 149.99, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(32, "premium", 149.99, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(33, "premium", 149.99, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(34, "premium", 149.99, 2));
+    oUPOCampo.altaHabitacion(new Habitacion(35, "premium", 149.99, 2));
+}
+
 
 //Datos pruebas Parking
 
-oUPOCampo.altaParking(new Parking(0, 25, false));
-oUPOCampo.altaParking(new Parking(1, 25, true));
-oUPOCampo.altaParking(new Parking(2, 25, false));
-oUPOCampo.altaParking(new Parking(3, 25, true));
-oUPOCampo.altaParking(new Parking(4, 25, false));
-oUPOCampo.altaParking(new Parking(5, 25, true));
-oUPOCampo.altaParking(new Parking(6, 25, false));
-oUPOCampo.altaParking(new Parking(7, 25, true));
-oUPOCampo.altaParking(new Parking(8, 25, false));
-oUPOCampo.altaParking(new Parking(9, 25, true));
-oUPOCampo.altaParking(new Parking(10, 25, false));
-oUPOCampo.altaParking(new Parking(11, 25, true));
-oUPOCampo.altaParking(new Parking(12, 25, false));
-oUPOCampo.altaParking(new Parking(13, 25, true));
-oUPOCampo.altaParking(new Parking(14, 25, false));
-oUPOCampo.altaParking(new Parking(15, 25, true));
-oUPOCampo.altaParking(new Parking(16, 25, false));
-oUPOCampo.altaParking(new Parking(17, 25, true));
-oUPOCampo.altaParking(new Parking(18, 25, false));
-oUPOCampo.altaParking(new Parking(19, 25, true));
-oUPOCampo.altaParking(new Parking(20, 25, true));
+function datosParking() {
+    oUPOCampo.altaParking(new Parking(0, 25, false));
+    oUPOCampo.altaParking(new Parking(1, 25, true));
+    oUPOCampo.altaParking(new Parking(2, 25, false));
+    oUPOCampo.altaParking(new Parking(3, 25, true));
+    oUPOCampo.altaParking(new Parking(4, 25, false));
+    oUPOCampo.altaParking(new Parking(5, 25, true));
+    oUPOCampo.altaParking(new Parking(6, 25, false));
+    oUPOCampo.altaParking(new Parking(7, 25, true));
+    oUPOCampo.altaParking(new Parking(8, 25, false));
+    oUPOCampo.altaParking(new Parking(9, 25, true));
+    oUPOCampo.altaParking(new Parking(10, 25, false));
+    oUPOCampo.altaParking(new Parking(11, 25, true));
+    oUPOCampo.altaParking(new Parking(12, 25, false));
+    oUPOCampo.altaParking(new Parking(13, 25, true));
+    oUPOCampo.altaParking(new Parking(14, 25, false));
+    oUPOCampo.altaParking(new Parking(15, 25, true));
+    oUPOCampo.altaParking(new Parking(16, 25, false));
+    oUPOCampo.altaParking(new Parking(17, 25, true));
+    oUPOCampo.altaParking(new Parking(18, 25, false));
+    oUPOCampo.altaParking(new Parking(19, 25, true));
+    oUPOCampo.altaParking(new Parking(20, 25, true));
+}
+
 
 //Datos pruebas Reguime alimenticio
-oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(0, 12.50));
-oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(1, 8.99));
-oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(2, 22.50));
-oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(3, 55.75));
-oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(4, 5));
-oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(6, 25));
+
+function datosRegimen() {
+    oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(0, 12.50));
+    oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(1, 8.99));
+    oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(2, 22.50));
+    oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(3, 55.75));
+    oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(4, 5));
+    oUPOCampo.altaRegimenAlimenticio(new RegimenAlimenticio(6, 25));
+}
+
 
 //Datos pruebas clientes
-oUPOCampo.altaCliente(new Cliente("25518526A", "Sofia", 34652753182, "Plaza roma s/n 13 2C", "sofialadivina@gmail.com", 4125825674593258));
-oUPOCampo.altaCliente(new Cliente("6298+369F", "Pedro", 34625635851, "Calle paraiso n5     ", "pedroelcrak@gmail.com  ", 4348521495426582));
-oUPOCampo.altaCliente(new Cliente("51483972H", "Raul ", 34785215953, "Av montequinto n5 ", "raulinformatico@gmail.com", 4654853696265553));
-oUPOCampo.altaCliente(new Cliente("25584568J", "Sara ", 34652983541, "plaza de parma 19 4A", "sara089@gmail.com", 4253852495328512));
-oUPOCampo.altaCliente(new Cliente("28569535T", "Paula", 34685257548, "Avenida alegria n3", "paula1999@hotmail.es",4582468528569514));
-oUPOCampo.altaCliente(new Cliente("85216328G", "Javier", 34655329357, "Plaza la nueva n5", "xavierfotografo@gmail.com", 4265582695478582));
-oUPOCampo.altaCliente(new Cliente("44855685D", "Rocio", 34685248263, "Av europa 5 3D", "rociobestfriends@gmail.com", 4885963258452658));
-oUPOCampo.altaCliente(new Cliente("54664665A", "Manuel", 34685695247, "Av portimao n 12", "manoloeldelbombo@hotmail.es",4859632458215236));
-oUPOCampo.altaCliente(new Cliente("68546545G", "Amparo", 34758265925, "Calle la esperanza n 9", "amparo1993@gmail.com", 4782695321584582));
-oUPOCampo.altaCliente(new Cliente("98866866S", "Lucia", 34658951235, "Plaza de parma 21 3B", "luciabogados@gmail.com", 4859652148536258));
 
-
+function datosCliente() {
+    oUPOCampo.altaCliente(new Cliente("25518526A", "Sofia", 34652753182, "Plaza roma s/n 13 2C", "sofialadivina@gmail.com", 4125825674593258));
+    oUPOCampo.altaCliente(new Cliente("6298+369F", "Pedro", 34625635851, "Calle paraiso n5     ", "pedroelcrak@gmail.com  ", 4348521495426582));
+    oUPOCampo.altaCliente(new Cliente("51483972H", "Raul ", 34785215953, "Av montequinto n5 ", "raulinformatico@gmail.com", 4654853696265553));
+    oUPOCampo.altaCliente(new Cliente("25584568J", "Sara ", 34652983541, "plaza de parma 19 4A", "sara089@gmail.com", 4253852495328512));
+    oUPOCampo.altaCliente(new Cliente("28569535T", "Paula", 34685257548, "Avenida alegria n3", "paula1999@hotmail.es",4582468528569514));
+    oUPOCampo.altaCliente(new Cliente("85216328G", "Javier", 34655329357, "Plaza la nueva n5", "xavierfotografo@gmail.com", 4265582695478582));
+    oUPOCampo.altaCliente(new Cliente("44855685D", "Rocio", 34685248263, "Av europa 5 3D", "rociobestfriends@gmail.com", 4885963258452658));
+    oUPOCampo.altaCliente(new Cliente("54664665A", "Manuel", 34685695247, "Av portimao n 12", "manoloeldelbombo@hotmail.es",4859632458215236));
+    oUPOCampo.altaCliente(new Cliente("68546545G", "Amparo", 34758265925, "Calle la esperanza n 9", "amparo1993@gmail.com", 4782695321584582));
+    oUPOCampo.altaCliente(new Cliente("98866866S", "Lucia", 34658951235, "Plaza de parma 21 3B", "luciabogados@gmail.com", 4859652148536258));
+}
